7bc155c945b2e5b56e02ac391f50ee4964ebf4c2
Haswanth Tamil <haswanthtamilofficial@gmail.com>
2025-06-04 20:40:48 +0530

    Add favicon.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next"
 export const metadata: Metadata = {
   title: "CollabBoard",
   description: "All in one productivity app for teams.",
+  icons: {
+    icon: "/logo.png",
+  },
 }
 
 export default function RootLayout({
