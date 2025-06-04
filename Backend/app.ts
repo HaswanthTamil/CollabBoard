c25edbd115220cb9578d9cb55761deedbe7461d8
user <admin@glowsquid.me>
2025-06-04 17:06:55 +0100

    Added hono's JWT middleware and routes for login and refreshing token

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,29 +1,31 @@
-import { Hono } from 'hono';
-import { connectDB } from './db/database.ts';
-import { serve } from '@hono/node-server';
-import { PORT } from './config/index.ts'
+import { Hono } from "hono";
+import { connectDB } from "./db/database.ts";
+import { serve } from "@hono/node-server";
+import { PORT } from "./config/index.ts";
+import authenticationRoutes from "./routes/authentication.ts";
+import { JwtVariables } from "hono/jwt";
 
+const app = new Hono<{ Variables: JwtVariables }>().basePath("/api");
 
-const app = new Hono();
-
-app.get('/', (c) => c.text('🚀 CollabBoard Backend Running!'));
+app.get("/health", (c) => c.text("🚀 CollabBoard Backend Running!"));
+app.route("/", authenticationRoutes);
 
 async function start() {
-    try {
-        console.log('⏳ Connecting to MongoDB...');
-        await connectDB();
-        
-        console.log(`🚀 Starting server on port ${PORT}`);
-        serve(app);
+  try {
+    console.log("⏳ Connecting to MongoDB...");
+    await connectDB();
+
+    console.log(`🚀 Starting server on port ${PORT}`);
+    serve(app);
   } catch (err) {
-    console.error('❌ Error in start():', err);
+    console.error("❌ Error in start():", err);
     process.exit(1);
   }
 }
 
-start().catch(err => {
-    console.error('Fatal error starting app:', err);
-    process.exit(1);
+start().catch((err) => {
+  console.error("Fatal error starting app:", err);
+  process.exit(1);
 });
 
 export default app;
