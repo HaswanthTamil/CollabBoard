6df2cb6fee25bfbf3f47378a095077b485e186c5
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-06-06 14:41:05 +0200

    Added function to fetch single project from postgres database Changed the say data is sent back to user for security so it doesn't send back the hashed password

--- Backend/src/db/postgres/entities/project.ts
+++ Backend/src/db/postgres/entities/project.ts
@@ -5,20 +5,20 @@ import {
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
-} from 'typeorm'
-import type { User } from './user'
+} from "typeorm";
+import { User } from "./user";
 
-@Entity({ name: 'project' })
+@Entity({ name: "project" })
 export class Project {
-  @PrimaryGeneratedColumn('uuid')
-  id: string
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
   @Column()
-  name: string
+  name: string;
 
-  @ManyToOne('User', 'projects')
-  createdBy: User
+  @ManyToOne(() => User, (user) => user.projects)
+  createdBy: User;
 
   @CreateDateColumn()
-  createdAt: Date
+  createdAt: Date;
 }
