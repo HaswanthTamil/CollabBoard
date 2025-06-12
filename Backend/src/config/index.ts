5881db9fdc5582e11e38402db453a7529add5667
Oloruntobi <alademehintobi1@gmail.com>
2025-06-12 14:54:11 +0100

    Removed .env.example file and updated config/index.ts to throw errors if environment variables are not set.

--- Backend/src/config/index.ts
+++ Backend/src/config/index.ts
@@ -1,12 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in the .env file");
+}
+if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI is not defined in the .env file");
+}
+if (!process.env.POSTGRES_URI) {
+  throw new Error("POSTGRES_URI is not defined in the .env file");
+}
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const VERCEL_DEPLOYMENT = process.env.VERCEL_DEPLOYMENT ?? false;
-
 export const PORT = Number(process.env.PORT) || 3000;
-export const MONGO_URI = process.env.MONGO_URI!;
-export const JWT_SECRET = process.env.JWT_SECRET!;
 
-export const POSTGRES_URI = process.env.POSTGRES_URI!;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const MONGO_URI = process.env.MONGO_URI;
+export const POSTGRES_URI = process.env.POSTGRES_URI;
--- Backend/src/middleware/authentication.ts
+++ Backend/src/middleware/authentication.ts
@@ -1,5 +1,5 @@
 import { jwt } from "hono/jwt";
-import { HTTPException } from 'hono/http-exception';
+import { HTTPException } from "hono/http-exception";
 import { JWT_SECRET } from "../config";
 import { Context, Next } from "hono";
 import { AuthContext, JWTPayload } from "../types/authentication";
@@ -7,36 +7,38 @@ import postgresDataSource from "../db/postgres";
 import { User } from "../db/postgres/entities/user";
 
 export const jwtMiddleware = jwt({
-    secret: JWT_SECRET,
-    cookie: "accessToken",
+  secret: JWT_SECRET,
+  cookie: "accessToken",
 });
 
 export async function authMiddleware(c: Context, next: Next) {
-    try {
-        const payload = c.get("jwtPayload") as JWTPayload;
+  try {
+    const payload = c.get("jwtPayload") as JWTPayload;
 
-        if (!payload) {
-            throw new HTTPException(401, { message: "Invalid token" });
-        }
-
-        const userRepository = postgresDataSource.getRepository(User);
-        const user = await userRepository.findOne({
-            where: { id: payload.sub },
-        })
-
-        if (!user) {
-            throw new HTTPException(401, { message: "No user found matching the token" });
-        }
-
-        const authContext: AuthContext = { user, payload };
-        c.set("authContext", authContext);
-
-        await next();
-    } catch (error) {
-        if (error instanceof HTTPException) {
-            throw error;
-        }
-
-        throw new HTTPException(401, { message: "Authentication failed" });
+    if (!payload) {
+      throw new HTTPException(401, { message: "Invalid token" });
     }
-};
+
+    const userRepository = postgresDataSource.getRepository(User);
+    const user = await userRepository.findOne({
+      where: { id: payload.sub },
+    });
+
+    if (!user) {
+      throw new HTTPException(401, {
+        message: "No user found matching the token",
+      });
+    }
+
+    const authContext: AuthContext = { user, payload };
+    c.set("authContext", authContext);
+
+    await next();
+  } catch (error) {
+    if (error instanceof HTTPException) {
+      throw error;
+    }
+
+    throw new HTTPException(401, { message: "Authentication failed" });
+  }
+}
